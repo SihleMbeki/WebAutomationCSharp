8e5c754db608dfdce031dcd62dadf52e3759978e
Masakhane Matanzima <sihle.matanzima@gmail.com>
2023-02-22 21:20:12 +0000

    Added NLog

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CommonsUtility;
+using NLog.Web;
+using System.Reflection.Metadata;
 
-Console.WriteLine("Output:"+ RandomGenerator.RandomNumber(lenght: 5));
-Console.WriteLine("Output:" + RandomGenerator.RandomNumber());
-Console.WriteLine("Output:" + RandomGenerator.guid().ToString());
-Console.WriteLine("Output:" + RandomGenerator.dateRandom());
-Console.WriteLine("Output:" + DefaultNames.fileNames.Reports);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Output:" + RandomGenerator.RandomNumber(lenght: 5));
+        Console.WriteLine("Output:" + RandomGenerator.RandomNumber());
+        Console.WriteLine("Output:" + RandomGenerator.guid().ToString());
+        Console.WriteLine("Output:" + RandomGenerator.dateRandom());
+        Console.WriteLine("Output:" + DefaultNames.fileNames.Reports);
+
+        var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+        try
+        {
+            logger.Debug("Init main");
+            logger.Error("test");
+        }
+        catch (System.Exception)
+        {
+            NLog.LogManager.Shutdown();
+        }
+}
+}
